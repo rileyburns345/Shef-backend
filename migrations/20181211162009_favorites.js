d310654fca9bbeee048aa36fa1bbe691222ca4d6
Russ Storms <rstorms90@gmail.com>
2018-12-11 19:35:08 -0700

    added another foreign key I believe to be there

--- migrations/20181211162009_favorites.js
+++ migrations/20181211162009_favorites.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
     table.integer('user_id')
     table.foreign('user_id').references('users.id').onDelete('CASCADE')
     table.integer('recipe_id')
+    table.foreign('recipe_id').references('recipes.id').onDelete('CASCADE')
     table.timestamps(true, true)
   })
 }
