acf88738a09c894c910d04b08f556df07901fb12
Russ Storms <rstorms90@gmail.com>
2018-12-12 12:48:12 -0700

    fixed migrations

--- migrations/20181211162003_recipes.js
+++ migrations/20181211162003_recipes.js
@@ -2,7 +2,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('recipes', function(table) {
     table.increments()
     table.string('recipe_name', 255).defaultTo('')
-    table.integer('user_id').references('users.id').onDelete('CASCADE')
+    table.integer('user_id')
+    table.foreign('user_id').references('users.id').onDelete('CASCADE')
     table.string('image_url').defaultTo('https://placekitten.com/200/300')
     table.string('description').defaultTo('')
     table.string('instructions').defaultTo('')
