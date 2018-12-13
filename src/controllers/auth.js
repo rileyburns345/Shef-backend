73abfc6c5fe1361fa78cfadd158bf70b6824b699
Russ Storms <rstorms90@gmail.com>
2018-12-12 23:14:43 -0700

    hashing with bcrypt after verification

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,3 +11,29 @@ const tokenForUser = (user) => {
 const signin = (req, res, next) => {
   res.send({ token: tokenForUser(req.user) })
 }
+
+const signup = (req, res, next) => {
+
+  const {name, email, password} = req.body
+  const saltRounds = 12
+
+  if (!email || !password) {
+    res.status(422).send({error: `You must provide and email and a password.`})
+  }
+  //// SEE IF A USER WITH THE GIVEN EMAIL EXISTS \\\\
+  bcrypt.hash(password, saltRounds)
+    .then((hash) => {
+      return createUser(name, email, hash)
+        .then((newUser) => {
+          res.json({token: tokenForUser(newUser)})
+        })
+        .catch((err) => {
+          res.json({error: `Error saving user to database.`})
+        })
+    })
+    .catch((err) => {
+      return next(err)
+    })
+}
+
+module.exports = {signup, signin}
